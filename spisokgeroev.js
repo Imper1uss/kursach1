537ab31724ba76930bac6f45c717e58d4baaf984
Imper1uss <imper1uss666@gmail.com>
2018-11-26 16:39:56 +0200

    first commit

---
+++ spisokgeroev.js
@@ -0,0 +1,10 @@
+function border (element){
+    element.style.border="2px solid blue";
+    element.style.width='112px';
+    element.style.height='63px';
+}
+function notborder (element){
+    element.style.border="";
+    element.style.width="100px";
+    element.style.height="60px";
+}
